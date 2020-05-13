be053aebe5ba349556e25188b6db27889ccc3ae2
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-05-13 17:27:16 -0300

    Adicionando requisição post usando Volley.

--- app/src/main/java/com/ssi/audiotolibras/MainActivity.java
+++ app/src/main/java/com/ssi/audiotolibras/MainActivity.java
@@ -3,26 +3,43 @@ package com.ssi.audiotolibras;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     //inicializando o espaço para o texto retornar
     private TextView txtResult;
+    private TextView txtGlosa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtResult = (TextView) findViewById(R.id.txvResult);
+        txtGlosa = (TextView) findViewById(R.id.txtGlosa);
     }
 
     //ação que está associada a imagem do microfone
@@ -36,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
         //validando se o dispositivo suporta a operção
-        if (intent.resolveActivity(getPackageManager()) != null){
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(intent, 10);
-        }else{
+        } else {
             Toast.makeText(this, "Seu dispositivo não suporta Speech Input", Toast.LENGTH_SHORT).show();
         }
     }
@@ -47,14 +64,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode){
+        switch (requestCode) {
             case 10:
-                if(resultCode == RESULT_OK && data != null){
+                if (resultCode == RESULT_OK && data != null) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     txtResult.setText(result.get(0));
+                    post(result.get(0));
                 }
                 break;
         }
     }
 
+
+    //requisição post
+
+    public void post(String words) {
+        String url = "https://traducao2.vlibras.gov.br/translate";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONArray jsonArray = null;
+                        try {
+                            jsonArray = new JSONArray(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        JSONObject r = new JSONObject();
+                        JSONArray t;
+                        try {
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject palavras = jsonArray.getJSONObject(i);
+                                String frase = palavras.getString("text");
+                                txtGlosa.setText(frase);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d("Reponse", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error Response", error.toString());
+                    }
+                }
+
+        ) {
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("text:", "teste");
+                return params;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(postRequest);
+
+    }
 }
